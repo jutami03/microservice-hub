8456ed7420f5c1c7e2d4ceafa614bef938acdae7
Juliana Kanashiro <jutami03@gmail.com>
2026-03-06 11:26:11 -0300

    Primeiro commit

---
+++ src/test/java/com/github/jutami03/ms/pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jutami03.ms.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
